244b51baa81adddc7311e0dc980de414a8f4eba5
Valentin Belhomme <50097739+Valentinbel@users.noreply.github.com>
2024-01-27 14:47:11 +0100

    Projet vide

---
+++ src/main/java/com/escalabram/escalabram/EscalabramApplication.java
@@ -0,0 +1,13 @@
+package com.escalabram.escalabram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalabramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalabramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escalabram/escalabram/EscalabramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escalabram.escalabram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalabramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
