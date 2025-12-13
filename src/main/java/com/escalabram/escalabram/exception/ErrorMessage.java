23902c9b39c8db695cc49272452d7b7ba269a2eb
vbelhomm <valentinbelhomme@gmail.com>
2025-12-13 14:51:25 +0100

    feat(REFACTOR): ErrorMessage to record

--- src/main/java/com/escalabram/escalabram/exception/ErrorMessage.java
+++ src/main/java/com/escalabram/escalabram/exception/ErrorMessage.java
@@ -1,21 +1,10 @@
 package com.escalabram.escalabram.exception;
 
-import lombok.Getter;
-
 import java.util.Date;
 
-@Getter
-public class ErrorMessage { // todo record
-
-    private final int statusCode;
-    private final Date dateTime;
-    private final String message;
-    private final String description;
-
-    public ErrorMessage(int statusCode, Date dateTime, String message, String description) {
-        this.statusCode = statusCode;
-        this.dateTime = dateTime;
-        this.message = message;
-        this.description = description;
-    }
-}
+public record ErrorMessage(
+        int statusCode,
+        Date dateTime,
+        String message,
+        String description
+) {}
